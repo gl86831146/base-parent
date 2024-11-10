e892f1ea30787c80207dead9aaa6a856f57aadc5
gao <2813314009@qq.com>
2024-11-10 21:05:01 +0800

    菜单管理模块接口开发

---
+++ base-web/src/main/java/top/gsc/web/sys_menu/mapper/SysMenuMapper.java
@@ -0,0 +1,7 @@
+package top.gsc.web.sys_menu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import top.gsc.web.sys_menu.entity.SysMenu;
+
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+}
